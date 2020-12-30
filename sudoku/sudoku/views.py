7cb5c52552d2e57299303f062a77c94fe4a8d8d9
Poojith Reddy Rachakada <48828785+PoojithRachakada@users.noreply.github.com>
2020-12-30 20:11:17 +0530

    Add files via upload

---
+++ sudoku/sudoku/views.py
@@ -0,0 +1,37 @@
+from django.http import request
+from django.shortcuts import render
+import json
+from . import gensudoku
+
+
+def handler404(request):
+    response = render(
+        request,
+        "404.html",
+    )
+    return response
+
+
+def home(request):
+    return render(request, "index.html")
+
+
+
+
+def easy(request):
+
+    qs, ans = gensudoku.all()
+
+    arr = []
+    for i in range(9):
+        for j in range(9):
+            arr.append(qs[i][j])
+
+    json_list = json.dumps(arr)
+    return render(request, "sudoku.html", {"list": json_list})
+
+
+def medium(request):
+    print("selected medium level")
+
+    return render(request, "sudoku.html")
